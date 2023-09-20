eda2a5b2a8e30554c2c29e305be68d4575113299
kong <kong8715@gmail.com>
2023-09-21 00:54:50 +0900

    Persistence Layer 1차
    - product entity, repository, service, controller 생성

---
+++ src/main/java/simple/cafekiosk/spring/api/service/product/response/ProductResponse.java
@@ -0,0 +1,38 @@
+package simple.cafekiosk.spring.api.service.product.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import simple.cafekiosk.spring.domain.product.Product;
+import simple.cafekiosk.spring.domain.product.ProductSellingStatus;
+import simple.cafekiosk.spring.domain.product.ProductType;
+
+@Getter
+public class ProductResponse {
+    private Long id;
+    private String productNumber;
+    private ProductType type;
+    private ProductSellingStatus sellingStatus;
+    private String name;
+    private int price;
+
+    @Builder
+    private ProductResponse(Long id, String productNumber, ProductType type, ProductSellingStatus sellingStatus, String name, int price) {
+        this.id = id;
+        this.productNumber = productNumber;
+        this.type = type;
+        this.sellingStatus = sellingStatus;
+        this.name = name;
+        this.price = price;
+    }
+
+    public static ProductResponse of(Product product) {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .productNumber(product.getProductNumber())
+                .type(product.getType())
+                .sellingStatus(product.getSellingStatus())
+                .name(product.getName())
+                .price(product.getPrice())
+                .build();
+    }
+}
