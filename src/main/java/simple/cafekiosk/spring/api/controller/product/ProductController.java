18970669bb518e62fcf75f9178cb7ff529232abf
kong <kong8715@gmail.com>
2023-10-30 15:28:29 +0900

    Presentation Layer 테스트 - productController

--- src/main/java/simple/cafekiosk/spring/api/controller/product/ProductController.java
+++ src/main/java/simple/cafekiosk/spring/api/controller/product/ProductController.java
@@ -4,11 +4,14 @@ package simple.cafekiosk.spring.api.controller.product;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import simple.cafekiosk.spring.api.ApiResponse;
 import simple.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
 import simple.cafekiosk.spring.api.service.product.ProductService;
 import simple.cafekiosk.spring.api.service.product.response.ProductResponse;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -18,12 +21,12 @@ public class ProductController {
     private final ProductService productService;
 
     @PostMapping("/api/v1/products/new")
-    public void createProduct(ProductCreateRequest request) {
-        productService.createProduct(request);
+    public ApiResponse<ProductResponse> createProduct(@Valid @RequestBody ProductCreateRequest request) {
+        return  ApiResponse.ok(productService.createProduct(request));
     }
 
     @GetMapping("/api/v1/products/selling")
-    public List<ProductResponse> getSellingProducts() {
-        return productService.getSellingProducts();
+    public ApiResponse<List<ProductResponse>> getSellingProducts() {
+        return ApiResponse.ok(productService.getSellingProducts());
     }
 }
--- src/main/java/simple/cafekiosk/spring/api/controller/product/dto/request/ProductCreateRequest.java
+++ src/main/java/simple/cafekiosk/spring/api/controller/product/dto/request/ProductCreateRequest.java
@@ -2,16 +2,29 @@ package simple.cafekiosk.spring.api.controller.product.dto.request;
 
 import lombok.Builder;
 import lombok.Getter;
-import org.hibernate.engine.query.spi.ReturnMetadata;
+import lombok.RequiredArgsConstructor;
 import simple.cafekiosk.spring.domain.product.Product;
 import simple.cafekiosk.spring.domain.product.ProductSellingStatus;
 import simple.cafekiosk.spring.domain.product.ProductType;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
 @Getter
+@RequiredArgsConstructor
 public class ProductCreateRequest {
+
+    @NotNull(message = "상품 타입은 필수입니다.")
     private ProductType type;
+
+    @NotNull(message = "상품 판매상태는 필수입니다.")
     private ProductSellingStatus sellingStatus;
+
+    @NotBlank(message = "상품 이름은 필수입니다.")
     private String name;
+
+    @Positive(message = "상품 가격은 양수여야 합니다.")
     private int price;
 
     @Builder
